27a27b9e2ac20595fd8ecb3e5cf48bf7751a0f16
Patrick Allen <allenpatrick86@gmail.com>
2016-08-14 15:00:22 -0700

    add blast for all customers

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,9 +3,12 @@ class NotificationsController < ApplicationController
   skip_before_action :verify_authenticity_token
  
   def notify
-    client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    message = client.messages.create from: '+18582120341', to: '+16318382261', body: 'This is a super sweet test SMS'
-    render plain: message.status
+    Customer.all.each do |customer|
+      client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+      message = client.messages.create from: '+18582120341', to: "#{customer.phone}", body: "Hi #{customer.first_name}! This is a super sweet test SMS"
+      redirect_to root_path
+      # render plain: message.status (ORIGINAL SYNTAX)
+    end
   end
  
 end
