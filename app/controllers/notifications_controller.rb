1b7a05b4a40064de69fc157bb9df2a8cd82498de
Patrick Allen <allenpatrick86@gmail.com>
2016-08-14 15:14:59 -0700

    add custom message sending

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,12 +3,21 @@ class NotificationsController < ApplicationController
   skip_before_action :verify_authenticity_token
  
   def notify
+    phone = params[:phone]
+    body = params[:body]
+    client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+    message = client.messages.create from: '+18582120341', to: "+1#{phone}", body: body
+      # render plain: message.status (ORIGINAL SYNTAX)
+    redirect_to root_path
+  end
+
+  def blast
     Customer.all.each do |customer|
       client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
       message = client.messages.create from: '+18582120341', to: "#{customer.phone}", body: "Hi #{customer.first_name}! This is a super sweet test SMS"
-      redirect_to root_path
       # render plain: message.status (ORIGINAL SYNTAX)
     end
+    redirect_to root_path
   end
  
 end
