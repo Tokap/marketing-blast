a04488705cf66544e9374b79e8d6f88914780615
Patrick Allen <allenpatrick86@gmail.com>
2016-09-04 16:49:17 -0700

    add framework for tests

---
+++ spec/outreach_spec.rb
@@ -0,0 +1,6 @@
+require "rails_helper" 
+
+describe "outreach model" do
+  let(:outreach) { Outreach.new(id: 20)}
+
+end  
---
+++ spec/spreadsheet_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper" 
+
+describe "spreadsheet model" do
+
+  let(:spreadsheet) { Spreadsheet.new(id: 20)}
+
+end  
