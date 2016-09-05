246995fb24be08ef6d212e059457a6f448374f9b
Patrick Allen <allenpatrick86@gmail.com>
2016-09-04 19:05:40 -0700

    add tests for basic attributes

--- spec/outreach_spec.rb
+++ spec/outreach_spec.rb
@@ -1,6 +1,24 @@
 require "rails_helper" 
 
 describe "outreach model" do
-  let(:outreach) { Outreach.new(id: 20)}
+  let(:outreach) { Outreach.new(id: 20, name: "Test Template", image: "http://iambored.pro/wp-content/uploads/2014/04/funny-fun-fun-com.jpg", description: "Test outreach template.")}
+
+  describe "Gets initialized with basic attributes" do
+    it "has an ID" do
+      expect(outreach.id).to eq(20)
+    end
+
+    it "has a name" do
+      expect(outreach.name).to eq("Test Template")
+    end
+
+    it "has an image url" do
+      expect(outreach.image).to eq("http://iambored.pro/wp-content/uploads/2014/04/funny-fun-fun-com.jpg")
+    end
+
+    it "has a description" do
+      expect(outreach.description).to eq("Test outreach template.")
+    end
+  end
 
 end  
